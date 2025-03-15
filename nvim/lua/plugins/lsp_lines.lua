2d84f29b8b31f9db47cf7d704c773f5c9f020ef8
fvan-wij <fvan-wij@student.codam.nl>
2025-03-15 19:28:44 +0100

    Errors now only show up on hovered line

--- nvim/lua/plugins/lsp_lines.lua
+++ nvim/lua/plugins/lsp_lines.lua
@@ -7,7 +7,7 @@ return {
       -- Disable virtual_text since it's redundant due to lsp_lines
       vim.diagnostic.config({
         virtual_text = false,
-        virtual_lines = true,
+        virtual_lines = { only_current_line = true },
       })
 
       -- Add a keybinding to toggle lsp_lines
