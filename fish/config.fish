635b5a4904460cb2388ef1ed4ac61dca7eab124f
fvan-wij <vanwijkflip@gmail.com>
2025-03-17 13:28:33 +0100

    lsp_lines settings were overwriten by lazyvims lsp config

--- fish/config.fish
+++ fish/config.fish
@@ -14,35 +14,11 @@ if status is-interactive
         alias open="explorer.exe"
     end
 
-    function git_remote_to_web_url
-        set git_url (git config --get remote.origin.url)
-
-        if string match -q 'git@*' $git_url
-            set web_url (echo $git_url | sed 's/git@\(.*\):\(.*\)\.git/https:\/\/\1\/\2/')
-
-        else if string match -q 'https://*' $git_url
-            set web_url (echo $git_url | sed 's/\.git$//')
-        else
-            set web_url $git_url
-            echo "Warning: Unrecognized Git URL format. Opening original URL."
-        end
-        echo $web_url
-    end
-
-    function gb
-        set web_url (git_remote_to_web_url)
-        if test -n "$web_url"
-            echo "Opening: $web_url"
-            google-chrome --no-sandbox $web_url
-        else
-            echo "Error: Could not determine repository URL. Are you in a Git repository?"
-        end
-    end
-
     # Abbreviations
     abbr --add gall git add -A
     abbr --add gstatus git status
     abbr --add gco git checkout
     abbr --add gcm git commit -m
     abbr --add gpo git push origin
+    abbr --add toconf cd ~/.config
 end
--- nvim/lua/plugins/lsp_lines.lua
+++ nvim/lua/plugins/lsp_lines.lua
@@ -3,14 +3,6 @@ return {
     "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
     config = function()
       require("lsp_lines").setup()
-
-      -- Disable virtual_text since it's redundant due to lsp_lines
-      vim.diagnostic.config({
-        virtual_text = false,
-        virtual_lines = { only_current_line = true },
-      })
-
-      -- Add a keybinding to toggle lsp_lines
       vim.keymap.set("", "<Leader>ll", require("lsp_lines").toggle, { desc = "Toggle lsp_lines" })
     end,
   },
