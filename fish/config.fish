d8625dae20207eb0dec971ab597dfb061943693f
fvan-wij <vanwijkflip@gmail.com>
2025-03-17 11:15:34 +0100

    Updated dotfiles

--- fish/config.fish
+++ fish/config.fish
@@ -2,7 +2,13 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     ls
     alias xclip="xclip -selection clipboard"
-    alias open="explorer.exe"
     set -g fish_color_scheme fairground
     set -U fish_greeting ""
+    if test -f /proc/version && string match -q "*Linux*" (cat /proc/version)
+        alias open="xdg-open"
+    else
+        alias open="explorer.exe"
+    end
+    abbr --add gall git add -A
+    abbr --add gco git checkout
 end
