98ef67040aade9eae4fada4245b74f5ba077d2c7
fvan-wij <vanwijkflip@gmail.com>
2025-03-17 11:18:12 +0100

    Added comments

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,8 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+    # Commands on boot
     ls
+
+    # Aliases
     alias xclip="xclip -selection clipboard"
     set -g fish_color_scheme fairground
     set -U fish_greeting ""
@@ -9,6 +11,8 @@ if status is-interactive
     else
         alias open="explorer.exe"
     end
+
+    # Abbreviations
     abbr --add gall git add -A
     abbr --add gco git checkout
     abbr --add gcm git commit -m
